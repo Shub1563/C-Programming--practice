df87b79d8e80d67978d4901ed1543cc6ed9ae751
Shubh123464 <sy999840@gmail.com>
2026-07-04 10:11:13 +0530

    add calculating fractional part of number

---
+++ calculate fractional part of a number/calculating fractional part of number.c
@@ -0,0 +1,17 @@
+// take float input and  print fractional part of the number
+
+#include<stdio.h>
+int main(){
+    float in;
+    printf("enter the float input: ");
+    scanf("%f",&in);
+    int x=in;
+    float y=in-x;
+    printf("fractional part is:%f",y);
+     return 0;
+
+
+
+
+
+}
