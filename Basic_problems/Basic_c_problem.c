02dbef95aab1ac396b361ccfe06df8eceaf6d2cc
Shubh123464 <sy999840@gmail.com>
2026-07-04 09:53:47 +0530

    add basic c problem

---
+++ Basic_problems/Basic_c_problem.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+   //area of a square
+   float r;
+   printf("enter the value of radius\n");
+   scanf("%f",&r);
+   float area=3.14*r*r;
+
+   printf("area of the circle is %f",area);
+
+
+    return 0;
+}
