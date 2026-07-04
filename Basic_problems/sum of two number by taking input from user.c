44fbb6ff623932d30f77820ec14e0c59475bd60a
Shubh123464 <sy999840@gmail.com>
2026-07-04 10:07:09 +0530

    add basic c problem

---
+++ Basic_problems/sum of two number by taking input from user.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+   float a,b;
+   printf("enter the first number\n:");
+   scanf("%f",&a);
+   printf("enter the second number\n:");
+   scanf("%f",&b);
+
+   float sum=a+b;
+   printf("sum of two number is:%f",sum);
+
+    return 0;
+}
