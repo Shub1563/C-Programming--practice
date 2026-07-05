a529342e897637968de72ff304f601edd3766320
Shubham Yadav <sky571631@gmail.com>
2026-07-05 09:36:01 +0530

    Add even or odd checker for positive integers
    Implement a program to check if a number is even or odd.

---
+++ Conditional statement/conditional_statement_problem.c
@@ -0,0 +1,17 @@
+/*Take positive integer input and tell if it 
+is even or odd*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the number n:");
+    scanf("%d",&n);
+        if(n%2==0){
+
+            printf("number is even");
+        }
+    else{
+        printf("number is odd");
+    }
+
+ return 0;
+}
