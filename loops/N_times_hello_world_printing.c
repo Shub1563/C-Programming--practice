78ec59b399dd4c1026fea0eb7fc86d98f885424e
Shubham Yadav <sky571631@gmail.com>
2026-07-09 12:41:19 +0530

    Create N_times_hello_world_printing.c

---
+++ loops/N_times_hello_world_printing.c
@@ -0,0 +1,15 @@
+/*Print hello world ‘n’ times. Take ‘n’ as 
+input from user
+*/
+#include<stdio.h>
+int main(){
+      int n;
+      printf("enter the value of n:");
+      scanf("%d",&n);
+      for(int i=1;i<=n;i++){
+
+        printf("Helllo World\n");
+     }
+
+    return 0;
+}
